23264ab603dd363431f873bb24bcc27cd6fe4741
phher802 <pher024@gmail.com>
2021-04-19 23:33:01 -0500

    Add function ArticleInfo to ArticleInfo.js

--- src/components/articles/ArticleList.js
+++ src/components/articles/ArticleList.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
-import { useState } from 'react';
+
 
 function ArticleList(){
     const [articles, setArticles] = useState([]);
