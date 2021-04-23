8325c888e6556e448908a4f598facfb2a98030d3
phher802 <pher024@gmail.com>
2021-04-22 23:11:50 -0500

    Add ArticleEdit function to ArticleEdit.js

---
+++ src/components/articles/ArticleEdit.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import {get, patch} from 'axios';
+
+function ArticleEdit(){
+    const intialState = {language: '', category: '', title: '', content: ''}
+    const [article, setArticle] = useState(initialState);
+
+    useEffect(function(){
+        async function getArticle(){
+            try{
+                const response = await get('/api/articles/${props.match.params._id}');
+                setArticle(response.data);
+            }catch(error){
+                console.log(error);
+            }
+        }
+        getArticle();
+    }, [props]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        async function updateArticle(){
+            try{
+                await patch('/api/articles/${article._id}', article);
+                props.history.push('/articles/${article._id}');
+            }catch(error){
+                console.log(error);
+            }
+        }
+        updateArticle();
+    }
+    function handleChange(){
+        setArticle({...article, [event.target.name]: event.target.value})
+    }
+}
